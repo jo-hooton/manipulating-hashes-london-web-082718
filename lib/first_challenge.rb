7a4a77fd849a5bbd5c30b125651c471e407ed175
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-27 20:06:07 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-   contacts["Freddie Mercury"][favorite_icecream_flavors:].pop {"strawberry"}
+   contacts{["Freddie Mercury"][:favorite_icecream_flavors]}.pop "strawberry"
   #remember to return your newly altered contacts hash!
  contacts.each do |person, data|
   data.each do |attribute, value|
